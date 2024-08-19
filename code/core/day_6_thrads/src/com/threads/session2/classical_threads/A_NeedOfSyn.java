fda8ad71d06c7b72d61cb70c6444cde1306358f6
rgupta00 <rgupta.mtech@gmail.com>
2024-08-19 19:42:29 +0530

    notes added

---
+++ code/core/day_6_thrads/src/com/threads/session2/classical_threads/A_NeedOfSyn.java
@@ -0,0 +1,20 @@
+package com.threads.session2.classical_threads;
+//Printer ---> 3 employee : race condition
+
+class Printer {
+}
+
+class Client {
+	private String letter;
+	private Printer printer;
+	private Thread thread;
+
+}
+
+public class A_NeedOfSyn {
+	public static void main(String[] args) {
+
+		Printer printer = new Printer();
+
+	}
+}

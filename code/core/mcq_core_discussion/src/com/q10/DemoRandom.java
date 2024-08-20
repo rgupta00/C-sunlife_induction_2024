b385f120a601834b54140d89f097991acea11040
rgupta00 <rgupta.mtech@gmail.com>
2024-08-20 16:34:53 +0530

    day 6 mcq discussion:

---
+++ code/core/mcq_core_discussion/src/com/q10/DemoRandom.java
@@ -0,0 +1,9 @@
+package com.q10;
+
+public class DemoRandom {
+
+	public static void main(String[] args) {
+		int i = (int) Math.random() * 10;
+		System.out.println(i);
+	}
+}
---
+++ code/core/mcq_core_discussion/src/com/q2/Foof.java
@@ -0,0 +1,8 @@
+package com.q2;
+
+public class Foof {
+	public static void main(String str[]) {
+		String s = new String(null);
+		System.out.print(s);
+	}
+}

02c144ac9365a6ca55a244bb9ce43c12a2f80e0e
rgupta00 <rgupta.mtech@gmail.com>
2024-08-28 11:28:25 +0530

    gitignore

---
+++ training_code/module3_spring_boot/code/spring_core/01_spring_di_basics/src/main/java/com/demo/CeatTyre.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+public class CeatTyre implements Tyre {
+	@Override
+	public void rotate() {
+		System.out.println("ceat tyre is rotating");
+	}
+}
---
+++ training_code/module3_spring_boot/code/spring_core/01_spring_di_basics/src/main/java/com/demo/TurboEngine.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+public class TurboEngine implements Engine{
+	@Override
+	public void move() {
+		System.out.println("Turbo engine is working");
+	}
+}

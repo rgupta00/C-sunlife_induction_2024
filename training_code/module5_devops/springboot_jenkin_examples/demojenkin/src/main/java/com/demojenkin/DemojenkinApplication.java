fa631fc24b2e71c8efbeaf1e20161f0bb1fa81d8
rgupta00 <rgupta.mtech@gmail.com>
2024-09-20 16:23:15 +0530

    devops tools

---
+++ training_code/module5_devops/springboot_jenkin_examples/demojenkin/src/main/java/com/demojenkin/DemojenkinApplication.java
@@ -0,0 +1,13 @@
+package com.demojenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojenkinApplication.class, args);
+	}
+
+}
---
+++ training_code/module5_devops/testing_code_quality/productapp-test-todo/src/main/java/com/productapp/Productapp01Application.java
@@ -0,0 +1,24 @@
+package com.productapp;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class Productapp01Application implements CommandLineRunner {
+
+//	@Autowired
+//	private ProductRepo productRepo;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Productapp01Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//productRepo.save(new Product("laptop",120000));
+	}
+}
